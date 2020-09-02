d0450db93da74bb0f05b1a376059900b598226d4
AndreQuati <50759145+AndreQuati@users.noreply.github.com>
2020-09-02 20:17:56 +0900

    Mid of HTTP module

--- Section-9_HTTP-Requests/src/components/FullPost/FullPost.js
+++ Section-9_HTTP-Requests/src/components/FullPost/FullPost.js
@@ -1,20 +1,56 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
 
-        );
+    state = {
+        loadedPost: null
+    }
+
+    componentDidUpdate () {
+        // Checking if id is not null before sending the request
+        if(this.props.id) {
+        // componentDidUpdate is called every time the component is rendered, and since the
+        // state is being updated inside it, it will trigger the component to udpate every time
+        // this method is called, creating an infinit loop. 
+            // To avoid this issue, checking if this received post id is = this loadedPost id
+            // or if loadedPost = null, which means it hasn't been rendered yet
+            if(!this.state.loadedPost || this.props.id !== this.state.loadedPost.id) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                .then(response => {
+                    console.log(response.data);
+                    this.setState({loadedPost: response.data});
+                });
+            }
+        }
+    }
+
+    render () {
+        let post = <p style={{textAlign: "center"}}>Please select a Post!</p>;
+        
+        // ComponentDidUpdate is not called for the inital render and the API call is getting
+        // data asynchronously, so this component may try to render post while it's still null
+        // First, checking if the post id is null, if not it means there's a valid post,
+        // but it might not be retrieved from the API yet
+        if(this.props.id) {
+            post = <p style={{textAlign: "center"}}>Loading...</p>
+        }
+        // Second, checking if the full post's been loaded before rendering
+        if(this.state.loadedPost) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+    
+            );
+        }
+        
         return post;
     }
 }
--- Section-9_HTTP-Requests/src/containers/Blog/Blog.js
+++ Section-9_HTTP-Requests/src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null 
     }
 
     componentDidMount () {
@@ -17,14 +18,33 @@ class Blog extends Component {
             .then(response => {
                 // .data is the attribute from the json returned by the API that contains
                 // the array of posts
-                this.setState({posts: response.data});
-                console.log(response);
+                // Trimming the array to get only 4 posts 
+                const posts = response.data.slice(0,4);
+                // Creating a new post obj addin 'Author' attribute to the post
+                const updatedPosts = posts.map(post => {
+                    // For each post in Posts, creates a new object using the spread operator  
+                    // and adds the attribute 'author'
+                    return {
+                        ...post,
+                        author: 'Max'
+                    };
+                });
+                this.setState({posts: updatedPosts});
             });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
     }
 
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} />
+            return <Post 
+                key={post.id} 
+                title={post.title} 
+                author={post.author}
+                // clicked={() => this.postSelectedHandler(post.id)} />
+                clicked={() => this.postSelectedHandler(post.id)} />
         });
         return (
             <div>
@@ -32,7 +52,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
